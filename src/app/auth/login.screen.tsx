b9321adc322cfb0ae07a0ebcd9a11c494a313a19
Solex <solomonyunana95@gmail.com>
2021-12-03 07:45:46 +0100

    removed icons not app related

--- src/app/auth/login.screen.tsx
+++ src/app/auth/login.screen.tsx
@@ -191,12 +191,6 @@ class Login extends React.Component<Props> {
           customWavePattern="M0,160L48,154.7C96,149,192,139,288,149.3C384,160,480,192,576,218.7C672,245,768,267,864,277.3C960,288,1056,288,1152,266.7C1248,245,1344,203,1392,181.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
         />
         <View style={styles.flexContainer}>
-          <View style={styles.logoContainer}>
-            <Image
-              source={require('../../../assets/media/images/logo.png')}
-              style={styles.logo}
-            />
-          </View>
           <Container style={styles.mainContainer}>
             {this.props.errorLogin ? (
               <View style={styles.errorContainer}>
